90b8a05a9a055ecc6633121eebb11f853fc2859f
Rizzama <RZayed@aucegypt.edu>
2024-05-10 19:23:46 +0300

    Issue with the file resolved

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_cannonball.cpp
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_cannonball.cpp
@@ -37,7 +37,10 @@ struct qt_meta_stringdata_CLASSCannonballENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSCannonballENDCLASS = QtMocHelpers::stringData(
     "Cannonball",
     "movebullet",
-    ""
+    "",
+    "keyPressEvent",
+    "QKeyEvent*",
+    "event"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -50,7 +53,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSCannonballENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,10 +61,12 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSCannonballENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   20,    2, 0x0a,    1 /* Public */,
+       1,    0,   26,    2, 0x0a,    1 /* Public */,
+       3,    1,   27,    2, 0x0a,    2 /* Public */,
 
  // slots: parameters
     QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
 
        0        // eod
 };
@@ -76,7 +81,10 @@ Q_CONSTINIT const QMetaObject Cannonball::staticMetaObject = { {
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<Cannonball, std::true_type>,
         // method 'movebullet'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'keyPressEvent'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QKeyEvent *, std::false_type>
     >,
     nullptr
 } };
@@ -88,10 +96,10 @@ void Cannonball::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         (void)_t;
         switch (_id) {
         case 0: _t->movebullet(); break;
+        case 1: _t->keyPressEvent((*reinterpret_cast< std::add_pointer_t<QKeyEvent*>>(_a[1]))); break;
         default: ;
         }
     }
-    (void)_a;
 }
 
 const QMetaObject *Cannonball::metaObject() const
@@ -115,13 +123,13 @@ int Cannonball::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 2)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
